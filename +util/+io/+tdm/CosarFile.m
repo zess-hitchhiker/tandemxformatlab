0ce8a5703f47478e1c61e10d90a89e74b7c7ddcc
flobehner <behner@zess.uni-siegen.de>
2013-11-29 19:25:14 +0100

    Update CosarFile.m

--- +util/+io/+tdm/CosarFile.m
+++ +util/+io/+tdm/CosarFile.m
@@ -1,3 +1,9 @@
+%Copyright 2013 Florian Behner, Simon Reuter
+%This file is part of the TerraSAR-X/TanDEM-X Toolbox for MATLAB.
+%The TerraSAR-X/TanDEM-X Toolbox for MATLAB is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+%The TerraSAR-X/TanDEM-X Toolbox for MATLAB is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+%You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
+
 classdef CosarFile
     %CosarFile Representation of a *.cos-File
     %   Representation of a *.cos-File used for TerraSAR-X/TanDEM-X complex data
